5404bf8ae8f21e179c7c2f24c20030d07c3a801b
lisa850728g <lisa850728g@gmail.com>
2015-03-30 13:18:31 +0800

    update Account class
    新增ArrayList的物件，並且傳值進去
    新增註解

--- Account.java
+++ Account.java
@@ -1,14 +1,20 @@
 import java.util.*;
 
 class Account{
+	//create private variables
 	private int id = 0;
 	private String name = "C";
 	private double balance = 0;
 	private double annualInterestRate = 0;
 	private java.util.Date dateCreated;
 	
-	ArrayList<Transaction> transactions = new ArrayList<Transaction>();
-	
+	//new an object transactions, its type is ArrayList
+	ArrayList transactions = new ArrayList();
+	ArrayList list(){
+		return transactions;
+	}
+
+	//no-arg constructor
 	Account(){
 		id = 0;
 		name = "C";
@@ -16,6 +22,7 @@ class Account{
 		dateCreated = new java.util.Date();
 	}
 	
+	//constructor
 	Account(int newId , String newName , double newBalance){
 		id = newId;
 		name = newName;
@@ -23,57 +30,80 @@ class Account{
 		dateCreated = new java.util.Date();
 	}
 	
+	//get method of id
 	int getId(){
 		return id;
 	}
 	
+	//set method of id
 	void setId(int newId){
 		id = newId;
 	}
 	
+	//get method of name
 	String getName(){
 		return name;
 	}
 	
+	//set method of name
 	void setName(String newName){
 		name = newName;
 	}
 	
+	//get method of balance
 	double getBalance(){
 		return balance;
 	}
 	
+	//set method of balance
 	void setBalance(int newBalance){
 		balance = newBalance;
 	}
 	
+	//get method of annualInterestRate
 	double getAnnualInterestRate(){
 		return annualInterestRate ;
 	}
 	
+	//set method of annualInterestRate
 	void setAnnualInterestRate(double newAnnualInterestRate){
 		annualInterestRate = newAnnualInterestRate;
 	}
 	
+	//get method of dateCreated
 	java.util.Date getDateCreated(){
 		return dateCreated;
 	}
 	
+	//get method of monthlyInterestRate
 	double getMonthlyInterestRate(){
 		return annualInterestRate / 12 ;
 	}
 	
+	//get method of monthlyInterest
 	double getMonthlyInterest(){
 		return balance * annualInterestRate / 100 / 12;
 	}
 	
+	//withdraw method
 	void withdraw(double takeMoney){
+		//decrease balance
 		balance -= takeMoney;
-		transaction.add(withdraw(takeMoney));
+		//add an object in transactions
+		transactions.add(new Transaction('W',takeMoney,balance,"Thanks!"));
 	}
 	
+	//deposit method
 	void deposit(double putMoney){
+		//plus balance
 		balance += putMoney;
-		transaction.add(deposit(putMoney));
+		//add an object in transactions
+		transactions.add(new Transaction('D',putMoney,balance,"Thanks!"));
+	}
+	
+	//toString method
+	public String toString(){
+		return "\nName: " + getName() + "\nYour id: " + getId() +  "\nInterest rate: " + getAnnualInterestRate() +
+			"%\nBalance: " + getBalance();
 	}
 }
